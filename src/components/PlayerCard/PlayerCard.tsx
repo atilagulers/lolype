9f7f5a3173478e59d97ed4e6ba4e1c48eb03fba6
atilagulers <atilagulerofficial@gmail.com>
2023-08-14 00:18:44 +0300

    -

--- src/components/PlayerCard/PlayerCard.tsx
+++ src/components/PlayerCard/PlayerCard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Container, ToggleButton, Col, Image, Row} from 'react-bootstrap';
 import {Player} from '../../interfaces/interfaces';
 import './PlayerCard.scss';
+import SkillBar from '../SkillBar/SkillBar';
 
 interface PlayerCardProps {
   player: Player;
@@ -11,31 +12,35 @@ interface PlayerCardProps {
 function PlayerCard({player, handleChangeReady}: PlayerCardProps) {
   return (
     <Container className="player-card-container">
-      <Row>
+      <Row className="mb-5">
         <Col xs="2" lg="1" className="m-0 p-0">
-          <Image className="avatar" src="/champion.webp" rounded />
+          <Image className="selected-champion" src="/champion.webp" rounded />
         </Col>
-        <Col className="champion-name-container">
-          <label>Aatrox</label>
+        <Col xs="2" lg="2" className="">
+          <Row className="player-name-container">
+            <label id="name">{player?.name}</label>
+          </Row>
+          <Row className="champion-name-container">
+            <label>Aatrox</label>
+          </Row>
         </Col>
-        <Col>
-          <ToggleButton
-            className="ready-toggle"
-            id={`${player.name}-${player.isHost}`}
-            type="checkbox"
-            value="1"
-            variant="outline-primary"
-            checked={player?.isReady || false}
-            onChange={handleChangeReady}
-          >
-            Ready
-          </ToggleButton>
+        <Col lg="4">
+          <SkillBar />
         </Col>
+        <Col>Stats</Col>
       </Row>
       <Row>
-        <Col className="player-name-container">
-          <label id="name">{player?.name}</label>
-        </Col>
+        <ToggleButton
+          className="ready-toggle"
+          id={`${player.name}-${player.isHost}`}
+          type="checkbox"
+          value="1"
+          variant=""
+          checked={player?.isReady || false}
+          onChange={handleChangeReady}
+        >
+          Ready
+        </ToggleButton>
       </Row>
     </Container>
   );
---
+++ src/components/SkillBar/SkillBar.tsx
@@ -0,0 +1,47 @@
+import React, {useEffect} from 'react';
+import {Row, Col, Image} from 'react-bootstrap';
+import './SkillBar.scss';
+
+function SkillBar() {
+  return (
+    <Row className="skills-container">
+      <Col className="skill-column">
+        <Image
+          className="skill-image"
+          src="/champions/aatrox/skill-0.webp"
+          rounded
+        />
+      </Col>
+      <Col className="skill-column">
+        <Image
+          className="skill-image"
+          src="/champions/aatrox/skill-1.webp"
+          rounded
+        />
+      </Col>
+      <Col className="skill-column">
+        <Image
+          className="skill-image"
+          src="/champions/aatrox/skill-2.webp"
+          rounded
+        />
+      </Col>
+      <Col className="skill-column">
+        <Image
+          className="skill-image"
+          src="/champions/aatrox/skill-3.webp"
+          rounded
+        />
+      </Col>
+      <Col className="skill-column">
+        <Image
+          className="skill-image"
+          src="/champions/aatrox/skill-4.webp"
+          rounded
+        />
+      </Col>
+    </Row>
+  );
+}
+
+export default SkillBar;
